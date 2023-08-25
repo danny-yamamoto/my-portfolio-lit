485785a95beb365cfab7d8c7689cad1ab00a0355
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-08-25 12:13:43 +0000

    Add section.

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,7 +1,5 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
 
 /**
  * An example element.
@@ -25,29 +23,22 @@ export class MyElement extends LitElement {
 
   render() {
     return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
       <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
+        <section id="intro">
+          <p>Welcome to my portfolio</p>
+        </section>
+        <section id="experience">
+          <h2>Experience</h2>
+        </section>
+        <footer>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></link>
+          <p>&copy; 2023 hoge. All rights reserved.</p>
+          <div class="social-links">
+            <a href="https://github.com/danny-yamamoto" target="_blank"><i class="fab fa-github"></i></a>
+            <a href="https://twitter.com/dai_s_a_n" target="_blank"><i class="fab fa-twitter"></i></a>
+          </div>
+        </footer>
       </div>
-      <p class="read-the-docs">${this.docsHint}</p>
-      <footer>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></link>
-        <p>&copy; 2023 hoge. All rights reserved.</p>
-        <div>
-          <a href="https://github.com/danny-yamamoto" target="_blank"><i class="fab fa-github"></i></a>
-          <a href="https://twitter.com/dai_s_a_n" target="_blank"><i class="fab fa-twitter"></i></a>
-        </div>
-      </footer>
     `
   }
 
@@ -56,13 +47,14 @@ export class MyElement extends LitElement {
   }
 
   static styles = css`
+/*
     :host {
       max-width: 1280px;
       margin: 0 auto;
       padding: 2rem;
       text-align: center;
     }
-
+*/
     .logo {
       height: 6em;
       padding: 1.5em;
@@ -75,11 +67,11 @@ export class MyElement extends LitElement {
     .logo.lit:hover {
       filter: drop-shadow(0 0 2em #325cffaa);
     }
-
+/*
     .card {
       padding: 2em;
     }
-
+*/
     .read-the-docs {
       color: #888;
     }
@@ -124,6 +116,38 @@ export class MyElement extends LitElement {
       button {
         background-color: #f9f9f9;
       }
+    }
+    section {
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+    footer {
+      background-color: #333;
+      color: #fff;
+      text-align: center;
+      padding: 1rem 0;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+    }
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+      line-height: 1.6;
+    }
+    .social-links a {
+      color: #fff;
+      font-size: 1.5rem;
+      margin-left: 10px;
+      text-decoration: none;
+    }
+  
+    .social-links a:hover {
+        color: #0077cc;
     }
   `
 }
